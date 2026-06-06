5f856d8db838408cb34e68f5ed7e1496c7d295b5
zaursejfulaev <zaurseifulaev@yandex.ru>
2026-06-06 20:33:14 +0300

    Еще немного исправлений ошибок форматирования

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
-
 import java.time.LocalDate;
 
 /**
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,12 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-
 import java.time.LocalDate;
 
 @Data
-@Slf4j
 public class User {
     Long id;
     String email;
