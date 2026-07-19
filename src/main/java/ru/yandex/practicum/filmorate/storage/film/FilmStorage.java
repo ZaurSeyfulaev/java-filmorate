ce19af99ce4fa58bdfc350acaf69c054d6606498
zaursejfulaev <zaurseifulaev@yandex.ru>
2026-07-19 12:39:56 +0300

    Финальное задание 12 спринта 1. Правки стиля 2. Правки стиля 3. Правки стиля 4. Правки стиля

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,9 +4,9 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 import java.util.Optional;
+
 @Deprecated
 public interface FilmStorage {
-
     Collection<Film> getAllFilms();
 
     Film createFilm(Film film);
