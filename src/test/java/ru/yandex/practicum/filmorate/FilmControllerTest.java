243c2ae49631e3bd63e3aa934526d21b0925c2b7
zaursejfulaev <zaurseifulaev@yandex.ru>
2026-07-21 00:03:00 +0300

    Финальное задание 12 спринта Исправление замечаний по результатам ревью. Попытка 2 Правки стиля попытка 5

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
